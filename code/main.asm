7067e4c247d73f27047a43ad2ecf6ac3fbffc5d7
Conner Moulaison <csmoulaison@gmail.com>
2026-05-16 11:51:18 -0700

    more rendering notes and move comments to column 32

--- code/main.asm
+++ code/main.asm
@@ -9,7 +9,7 @@ section '.text' executable
 ; paygrade, but defining it here like this seems to work.
 public __dso_handle
 __dso_handle:
-	dd 0
+    dd 0
 
 ; glfw
 extrn glfwInit
@@ -53,276 +53,289 @@ extrn gl3wIsSupported
 
 public _start
 _start:
-	; Initialize glfw
-	call    glfwInit
-	cmp     eax, 1
-	jne     error
+    ; Initialize glfw
+    call    glfwInit
+    cmp     eax, 1
+    jne     error
 
-	mov     esi, 4
-	mov     edi, 0x00022002 ; GLFW_CONTEXT_VERSION_MAJOR
-	call    glfwWindowHint
-	mov     esi, 6
-	mov     edi, 0x00022003 ; GLFW_CONTEXT_VERSION_MINOR
-	call    glfwWindowHint
-	mov     esi, 0x00022008 ; GLFW_OPENGL_CORE_PROFILE
-	mov     edi, 0x00032001 ; GLFW_OPENGL_PROFILE
-	call    glfwWindowHint
+    mov     esi, 4
+    mov     edi, 0x00022002     ; GLFW_CONTEXT_VERSION_MAJOR
+    call    glfwWindowHint
+    mov     esi, 6
+    mov     edi, 0x00022003     ; GLFW_CONTEXT_VERSION_MINOR
+    call    glfwWindowHint
+    mov     esi, 0x00022008     ; GLFW_OPENGL_CORE_PROFILE
+    mov     edi, 0x00032001     ; GLFW_OPENGL_PROFILE
+    call    glfwWindowHint
 
-	mov     r8d, 0
-	mov     ecx, 0
-	mov     rdx, window_name
-	mov     esi, 480
-	mov     edi, 640
-	call    glfwCreateWindow
+    mov     r8d, 0
+    mov     ecx, 0
+    mov     rdx, window_name
+    mov     esi, 480
+    mov     edi, 640
+    call    glfwCreateWindow
 
-	mov     [glfw_window], rax
-	cmp     [glfw_window], 0
-	je      error
-	mov     rdi, [glfw_window]
-	call    glfwMakeContextCurrent
+    mov     [glfw_window], rax
+    cmp     [glfw_window], 0
+    je      error
+    mov     rdi, [glfw_window]
+    call    glfwMakeContextCurrent
 
-	; Initialize gl3w
-	call    gl3wInit
-	cmp     eax, 0
-	jne     error
+    ; Initialize gl3w
+    call    gl3wInit
+    cmp     eax, 0
+    jne     error
 
-	mov     esi, 3
-	mov     edi, 3
-	call    gl3wIsSupported
-	cmp     eax, 1
-	jne     error
+    mov     esi, 3
+    mov     edi, 3
+    call    gl3wIsSupported
+    cmp     eax, 1
+    jne     error
 
-	; Initialize OpenGL. We will need a texture and a quad to
-	; render across the screen as well as a shader program
+    ; Initialize OpenGL. We will need a texture and a quad to
+    ; render across the screen as well as a shader program
 
-	; Screen texture
-	mov     rsi, gl_texture
-	mov     rdi, 1
-	call    glGenTextures
+    ; Screen texture
+    mov     rsi, gl_texture
+    mov     rdi, 1
+    call    glGenTextures
 
-	mov     esi, [gl_texture]
-	mov     edi, 0x0DE1 ; GL_TEXTURE_2D
-	call    glBindTexture
+    mov     esi, [gl_texture]
+    mov     edi, 0x0DE1         ; GL_TEXTURE_2D
+    call    glBindTexture
 
-	mov     edx, 0x812F ; GL_CLAMP_TO_EDGE
-	mov     esi, 0x2802 ; GL_TEXTURE_WRAP_S
-	mov     edi, 0x0DE1 ; 0x0DE1 ; GL_TEXTURE_2D
-	call    glTexParameteri
-	mov     edx, 0x812F ; GL_CLAMP_TO_EDGE
-	mov     esi, 0x2803 ; GL_TEXTURE_WRAP_T
-	mov     edi, 0x0DE1 ; 0x0DE1 ; GL_TEXTURE_2D
-	call    glTexParameteri
-	mov     edx, 0x2600 ; GL_NEAREST
-	mov     esi, 0x2801 ; GL_TEXTURE_MIN_FILTER
-	mov     edi, 0x0DE1 ; GL_TEXTURE_2D
-	call    glTexParameteri
-	mov     edx, 0x2600 ; GL_NEAREST
-	mov     esi, 0x2800 ; GL_TEXTURE_MAG_FILTER
-	mov     edi, 0x0DE1 ; GL_TEXTURE_2D
-	call    glTexParameteri
+    mov     edx, 0x812F         ; GL_CLAMP_TO_EDGE
+    mov     esi, 0x2802         ; GL_TEXTURE_WRAP_S
+    mov     edi, 0x0DE1         ; 0x0DE1 ; GL_TEXTURE_2D
+    call    glTexParameteri
+    mov     edx, 0x812F         ; GL_CLAMP_TO_EDGE
+    mov     esi, 0x2803         ; GL_TEXTURE_WRAP_T
+    mov     edi, 0x0DE1         ; 0x0DE1 ; GL_TEXTURE_2D
+    call    glTexParameteri
+    mov     edx, 0x2600         ; GL_NEAREST
+    mov     esi, 0x2801         ; GL_TEXTURE_MIN_FILTER
+    mov     edi, 0x0DE1         ; GL_TEXTURE_2D
+    call    glTexParameteri
+    mov     edx, 0x2600         ; GL_NEAREST
+    mov     esi, 0x2800         ; GL_TEXTURE_MAG_FILTER
+    mov     edi, 0x0DE1         ; GL_TEXTURE_2D
+    call    glTexParameteri
 
-	; Quad mesh
-	mov     rsi, gl_vao
-	mov     edi, 1
-	call    glGenVertexArrays
+    ; Quad mesh
+    mov     rsi, gl_vao
+    mov     edi, 1
+    call    glGenVertexArrays
 
-	mov     edi, [gl_vao]
-	call    glBindVertexArray
+    mov     edi, [gl_vao]
+    call    glBindVertexArray
 
-	sub     rsp, 16 ; make space for vbo pointer
-	lea     rsi, [rsp] ; [rsp] = vbo
-	mov     edi, 1
-	call    glGenBuffers
+    sub     rsp, 16             ; make space for vbo pointer
+    lea     rsi, [rsp]          ; [rsp] = vbo
+    mov     edi, 1
+    call    glGenBuffers
 
-	mov     esi, [rsp]
-	mov     edi, 0x8892 ; GL_ARRAY_BUFFER
-	call    glBindBuffer
-	add     rsp, 16 ; don't need vbo anymore
+    mov     esi, [rsp]
+    mov     edi, 0x8892         ; GL_ARRAY_BUFFER
+    call    glBindBuffer
+    add     rsp, 16             ; don't need vbo anymore
 
-	mov     ebx, [verts_len]
-	mov     ebp, 4
-	; TODO: learn about mul and imul. something something lower bits
-	imul    ebp, ebx
-	mov     ecx, 0x88E4 ; GL_STATIC_DRAW
-	mov     rdx, verts
-	mov     esi, ebp
-	mov     edi, 0x8892 ; GL_ARRAY_BUFFER
-	call    glBufferData
+    mov     ebx, [verts_len]
+    mov     ebp, 4
+    ; TODO: learn about mul and imul. something something lower bits
+    imul    ebp, ebx
+    mov     ecx, 0x88E4         ; GL_STATIC_DRAW
+    mov     rdx, verts
+    mov     esi, ebp
+    mov     edi, 0x8892         ; GL_ARRAY_BUFFER
+    call    glBufferData
 
-	mov     edi, 0
-	call    glEnableVertexAttribArray
+    mov     edi, 0
+    call    glEnableVertexAttribArray
 
-	mov     r9d, 0
-	mov     r8d, 8
-	mov     ecx, 0
-	mov     edx, 0x1406 ; GL_FLOAT
-	mov     esi, 2
-	mov     edi, 0
-	call    glVertexAttribPointer
+    mov     r9d, 0
+    mov     r8d, 8
+    mov     ecx, 0
+    mov     edx, 0x1406         ; GL_FLOAT
+    mov     esi, 2
+    mov     edi, 0
+    call    glVertexAttribPointer
 
-	; Shader program
-	call    glCreateProgram
-	mov     [gl_program], eax ; program id
+    ; Shader program
+    call    glCreateProgram
+    mov     [gl_program], eax   ; program id
 
-	mov     rdx, vert_src_ptr
-	mov     rsi, vert_src_len
-	mov     edi, 0x8B31 ; GL_VERTEX_SHADER
-	call    compile_shader
-	mov     r12d, eax ; vert shader id
+    mov     rdx, vert_src_ptr
+    mov     rsi, vert_src_len
+    mov     edi, 0x8B31         ; GL_VERTEX_SHADER
+    call    compile_shader
+    mov     r12d, eax           ; vert shader id
 
-	mov     rdx, frag_src_ptr
-	mov     rsi, frag_src_len
-	mov     edi, 0x8B30 ; GL_FRAGMENT_SHADER
-	call    compile_shader
-	mov     r13d, eax ; frag shader id
+    mov     rdx, frag_src_ptr
+    mov     rsi, frag_src_len
+    mov     edi, 0x8B30         ; GL_FRAGMENT_SHADER
+    call    compile_shader
+    mov     r13d, eax           ; frag shader id
 
-	mov     edi, [gl_program]
-	call    glLinkProgram
+    mov     edi, [gl_program]
+    call    glLinkProgram
 
-	mov     edi, r12d
-	call    glDeleteShader
-	mov     edi, r13d
-	call    glDeleteShader
-	; r12, r13 are free for use
+    mov     edi, r12d           ; after deleting shaders, r12, r13 are free
+    call    glDeleteShader
+    mov     edi, r13d
+    call    glDeleteShader 
 
 ;===========================================================
 ; MAIN LOOP:
 ; This runs repeatedly until the program wants to exit
 loop_begin:
-	mov     rdi, [glfw_window]
-	call    glfwWindowShouldClose
-	cmp     eax, 1
-	je      exit
+    mov     rdi, [glfw_window]
+    call    glfwWindowShouldClose
+    cmp     eax, 1
+    je      exit
 
-;==========================================================
+;===========================================================
 ; RENDER PROCEDURE
+;
 ; At a high level, our goal is such:
-; for each pixel {
+; for each pixel
 ;    map pixel to a viewport position
-;    cast ray from origin through viewport
-;    for each cube {
-;        determine if cube intersects
-;        store cube color
-;    } (summing colors per transparency)
-;    write pixel to buffer
-; }
-	mov     rdi, 0
+;    cast ray from origin through viewport position
+;    for each cube
+;        determine if cube intersects with ray
+;        add cube color to sum
+;    write summed color to pixel buffer
+;
+; More specifically:
+; Implicitly, we have a camera centered at (0,0,0) pointing
+; down the +Z axis, with +Y above and +X to the right. We
+; have a viewport at some viewport_distance down the +Z
+; axis.
+;
+; For each (x,y) pixel, we get the position on the viewport
+; by scaling and adding x and y vectors along the viewport
+; by the same ratio of pixel to screen size. We then add a
+; random amount to the resulting viewport position such
+; that our resulting position is
+
+    mov     rdi, 0
 pixel_loop_begin:
-	; Determine pixel position and color
+    ; Determine pixel position and color
 
-	; TODO: In both of these division operations, we are moving
-	; the result we want into the register we want manually. Is
-	; there a better way to do this with less instructions?
-	mov     rax, rdi
-	mov     rsi, logical_w
-	mov     rdx, 0
-	div     rsi
-	mov     rsi, rdx ; x = (rdi % w)
+    ; TODO: In both of these division operations, we are
+    ; moving the result we want into the register we want
+    ; manually. Is there a better way to do this with less
+    ; instructions?
+    mov     rax, rdi
+    mov     rsi, logical_w
+    mov     rdx, 0
+    div     rsi
+    mov     rsi, rdx            ; x = (rdi % w)
 
-	mov     rax, rdi
-	mov     rcx, logical_w
-	div     rcx
-	mov     rcx, rax ; y = (rdi / w)
+    mov     rax, rdi
+    mov     rcx, logical_w
+    div     rcx
+    mov     rcx, rax            ; y = (rdi / w)
 
-	add     rsp, 4 ; make space for pixel color
-	mov     byte [rsp+0], 0xff
-	mov     byte [rsp+1], sil
-	mov     byte [rsp+2], cl
-	mov     byte [rsp+3], 0xff
-	mov     ebx, [rsp] ; pixel color
-	sub     rsp, 4 ; no need for rsp space anymore
+    add     rsp, 4              ; reserve 32 bits for color
+    mov     byte [rsp+0], 0xff
+    mov     byte [rsp+1], sil
+    mov     byte [rsp+2], cl
+    mov     byte [rsp+3], 0xff
+    mov     ebx, [rsp]          ; ebx now has color
+    sub     rsp, 4              ; free color bits
 
-	; Write color to position
-	xor     rax, rax ; clear rax for mul
-	mov     eax, logical_w
-	mul     rcx
-	mov     rcx, rax
-	add     rcx, rsi ; rcx is now (y * width + x)
-	mov     rax, 4
-	mul     rcx ; multiply r10 by color channels (4)
-	lea     r9, [pixels+rax] ; r9 now pointing to screen pixel
+    ; Write color to position
+    xor     rax, rax            ; clear rax for mul
+    mov     eax, logical_w
+    mul     rcx
+    mov     rcx, rax
+    add     rcx, rsi            ; rcx is now (y * width + x)
+    mov     rax, 4
+    mul     rcx                 ; multiply r10 by color channels (4)
+    lea     r9, [pixels+rax]    ; r9 now pointing to screen pixel
 
-	mov     dword [r9], ebx; Write pixel
+    mov     dword [r9], ebx     ; Write pixel
 
-	inc     rdi
-	cmp     rdi, pixels_len
-	jl      pixel_loop_begin
+    inc     rdi
+    cmp     rdi, pixels_len
+    jl      pixel_loop_begin
 
-	; Update pixels
-	mov     edx, 0xffff0000
-	mov     rsi, 50
-	mov     rdi, [player_x]
-	call    put_color
-	inc     [player_x]
+    ; Update pixels
+    mov     edx, 0xffff0000
+    mov     rsi, 50
+    mov     rdi, [player_x]
+    call    put_color
+    inc     [player_x]
 
 ;===========================================================
 ; UPDATE GL DATA
 
-	push    0
-	push    pixels
-	push    0x1401 ; GL_UNSIGNED_BYTE
-	push    0x1908 ; GL_RGBA
-	mov     r9d, 0
-	mov     r8d, logical_h
-	mov     ecx, logical_w
-	mov     edx, 0x1908 ; GL_RGBA
-	mov     esi, 0
-	mov     edi, 0x0DE1 ; GL_TEXTURE_2D
-	call    glTexImage2D
-	add     rsp, 32
 
-	sub     rsp, 16 ; make space for framebuffer dimensions
-	lea     rdx, [rsp+0x00] ; width
-	lea     rsi, [rsp+0x08] ; height
-	mov     rdi, [glfw_window]
-	call    glfwGetFramebufferSize
+    push    0
+    push    pixels
+    push    0x1401              ; GL_UNSIGNED_BYTE
+    push    0x1908              ; GL_RGBA
+    mov     r9d, 0
+    mov     r8d, logical_h
+    mov     ecx, logical_w
+    mov     edx, 0x1908         ; GL_RGBA
+    mov     esi, 0
+    mov     edi, 0x0DE1         ; GL_TEXTURE_2D
+    call    glTexImage2D
+    add     rsp, 32
 
-	mov     rcx, [rsp+0x00]
-	mov     rdx, [rsp+0x08]
-	mov     rsi, 0
-	mov     rdi, 0
-	call    glViewport
-	add     rsp, 16
+    sub     rsp, 16             ; make space for framebuffer dimensions
+    lea     rdx, [rsp+0x00]     ; width
+    lea     rsi, [rsp+0x08]     ; height
+    mov     rdi, [glfw_window]
+    call    glfwGetFramebufferSize
 
-	movss   xmm3, [clear_a]
-	movss   xmm2, [clear_b]
-	movss   xmm1, [clear_g]
-	movss   xmm0, [clear_r]
-	call    glClearColor
-	mov     edi, 0x00004000 ; GL_COLOR_BUFFER_BIT
-	call    glClear
+    mov     rcx, [rsp+0x00]
+    mov     rdx, [rsp+0x08]
+    mov     rsi, 0
+    mov     rdi, 0
+    call    glViewport
+    add     rsp, 16
 
-	mov     edi, [gl_program]
-	call    glUseProgram
+    movss   xmm3, [clear_a]
+    movss   xmm2, [clear_b]
+    movss   xmm1, [clear_g]
+    movss   xmm0, [clear_r]
+    call    glClearColor
+    mov     edi, 0x00004000     ; GL_COLOR_BUFFER_BIT
+    call    glClear
 
-	mov     esi, [gl_texture]
-	mov     edi, 0x0DE1 ; GL_TEXTURE_2D
-	call    glBindTexture
+    mov     edi, [gl_program]
+    call    glUseProgram
 
-	mov     edi, [gl_vao]
-	call    glBindVertexArray
+    mov     esi, [gl_texture]
+    mov     edi, 0x0DE1         ; GL_TEXTURE_2D
+    call    glBindTexture
 
-	mov     edx, [verts_len]
-	mov     esi, 0
-	mov     edi, 0x0004 ; GL_TRIANGLES
-	call    glDrawArrays
+    mov     edi, [gl_vao]
+    call    glBindVertexArray
+
+    mov     edx, [verts_len]
+    mov     esi, 0
+    mov     edi, 0x0004         ; GL_TRIANGLES
+    call    glDrawArrays
 
 ;===========================================================
 ; LOOP END
 
-	mov     rdi, [glfw_window]
-	call    glfwSwapBuffers
-	call    glfwPollEvents
-	jmp     loop_begin
+    mov     rdi, [glfw_window]
+    call    glfwSwapBuffers
+    call    glfwPollEvents
+    jmp     loop_begin
 
 ; TODO: Implement error messages
 error:
-	jmp     exit
+    jmp     exit
 
 exit:
-	call    glfwTerminate
-	mov     rax, 60 ; exit
-	xor     rdi, rdi
+    call    glfwTerminate
+    mov     rax, 60             ; exit
+    xor     rdi, rdi
     syscall 
 
 ; ==========================================================
@@ -335,53 +348,53 @@ exit:
 ; output
 ;   rax: shader id
 compile_shader:
-	sub     rsp, 40
-	mov     [rsp+0x00], rsi ; src len address
-	mov     [rsp+0x08], rdx ; src address ptr
+    sub     rsp, 40
+    mov     [rsp+0x00], rsi     ; src len address
+    mov     [rsp+0x08], rdx     ; src address ptr
 
-	call    glCreateShader ; type already in edi
-	mov     [rsp+0x10], rax ; shader id
+    call    glCreateShader      ; type already in edi
+    mov     [rsp+0x10], rax     ; shader id
 
-	mov     rcx, [rsp+0x00]
-	mov     rdx, [rsp+0x08]
-	mov     rsi, 1
-	mov     rdi, [rsp+0x10]
-	call    glShaderSource
+    mov     rcx, [rsp+0x00]
+    mov     rdx, [rsp+0x08]
+    mov     rsi, 1
+    mov     rdi, [rsp+0x10]
+    call    glShaderSource
 
-	mov     rdi, [rsp+0x10]
-	call    glCompileShader
+    mov     rdi, [rsp+0x10]
+    call    glCompileShader
 
-	lea     rdx, [rsp+0x18] ; compilation success flag
-	mov     rsi, 0x8B81 ; GL_COMPILE_STATUS
-	mov     rdi, [rsp+0x10]
-	call    glGetShaderiv
+    lea     rdx, [rsp+0x18]     ; compilation success flag
+    mov     rsi, 0x8B81         ; GL_COMPILE_STATUS
+    mov     rdi, [rsp+0x10]
+    call    glGetShaderiv
 
-	cmp     qword [rsp+0x18], 0
-	jne     compile_shader_success
+    cmp     qword [rsp+0x18], 0
+    jne     compile_shader_success
 
-	; Log error if shader compilation failed
-	mov     rcx, msg
-	mov     rdx, 0
-	mov     rsi, msglen
-	mov     rdi, [rsp+0x10]
-	call    glGetShaderInfoLog
+    ; Log error if shader compilation failed
+    mov     rcx, msg
+    mov     rdx, 0
+    mov     rsi, msglen
+    mov     rdi, [rsp+0x10]
+    call    glGetShaderInfoLog
 
-	mov     rax, 1 ; write syscall
-	mov     rdi, 1 ; stdout
-	mov     rsi, msg 
-	mov     rdx, msglen 
-	syscall
+    mov     rax, 1              ; write syscall
+    mov     rdi, 1              ; stdout
+    mov     rsi, msg
+    mov     rdx, msglen
+    syscall
 
-	jmp     exit
+    jmp     exit
 
 compile_shader_success:
-	mov     esi, [rsp+0x10]
-	mov     edi, [gl_program]
-	call    glAttachShader
+    mov     esi, [rsp+0x10]
+    mov     edi, [gl_program]
+    call    glAttachShader
 
-	mov     rax, [rsp+0x10] ; return shader id
-	add     rsp, 40
-	ret
+    mov     rax, [rsp+0x10]     ; return shader id
+    add     rsp, 40
+    ret
 
 ;===========================================================
 ; Writes an rgb value to the pixels buffer.
@@ -395,20 +408,20 @@ compile_shader_success:
 ;   rdi: x
 ; output: none
 put_color:
-	add     rsp, 8
-	mov     [rsp], edx
-	mov     r10, rsi
-	xor     rax, rax
-	mov     eax, logical_w
-	mul     r10
-	mov     r10, rax
-	add     r10, rdi ; r10 is now (y * width + x)
-	mov     rax, 4
-	mul     r10 ; multiply r10 by color channels (4)
-	lea     r9, [pixels+rax] ; r9 now pointing to screen pixel
-	mov     r11, [rsp]
-	mov     dword [r9], r11d ; Write pixel
-	sub     rsp, 8
+    add     rsp, 8
+    mov     [rsp], edx
+    mov     r10, rsi
+    xor     rax, rax
+    mov     eax, logical_w
+    mul     r10
+    mov     r10, rax
+    add     r10, rdi            ; r10 is now (y * width + x)
+    mov     rax, 4
+    mul     r10                 ; multiply r10 by color channels (4)
+    lea     r9, [pixels+rax]    ; r9 now pointing to screen pixel
+    mov     r11, [rsp]
+    mov     dword [r9], r11d    ; Write pixel
+    sub     rsp, 8
 
 ;===========================================================
 section '.data' writeable
@@ -419,7 +432,7 @@ logical_w  = 128
 logical_h  = 128
 pixels_len = logical_w * logical_h
 
-msg         rd msglen; general purpose string buffer
+msg         rd msglen           ; general purpose string buffer
 window_name db 'Cube Games', 0
 
 ; game state
